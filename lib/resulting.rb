d280e7eddd87ca73df0575510fa9792792f44316
Mislav Marohnić <mislav.marohnic@gmail.com>
2009-03-17 18:37:17 +0100

    cleanup RSpactorFormatter by inheriting from Spec::Runner::Formatter::BaseFormatter

--- lib/resulting.rb
+++ lib/resulting.rb
@@ -1,43 +1,9 @@
-class RSpactorFormatter
-  attr_accessor :example_group, :options, :where
-  def initialize(options, where)
-    @options = options
-    @where = where
-  end
-  
-  def dump_summary(duration, example_count, failure_count, pending_count)
-    img = (failure_count == 0) ? "rails_ok.png" : "rails_fail.png"
-    growl "Test Results", "#{example_count} examples, #{failure_count} failures", File.dirname(__FILE__) + "/../images/#{img}", 0
-  end
+require 'spec/runner/formatter/base_formatter'
 
-  def start(*ignore_these)
-  end
-
-  def add_example_group(*ignore_these)
-  end
-
-  def example_started(*ignore_these)
-  end
-
-  def example_passed(*ignore_these)
-  end
-
-  def example_failed(*ignore_these)
-  end
-  
-  def example_pending( *ignore_these) 
-  end
-
-  def start_dump
-  end
-
-  def dump_failure(*ignore_these)
-  end
-
-  def dump_pending
-  end
-
-  def close
+class RSpactorFormatter < Spec::Runner::Formatter::BaseFormatter
+  def dump_summary(duration, total, failures, pending)
+    image_path = File.dirname(__FILE__) + "/../images/rails_#{failures.zero?? 'ok' : 'fail'}.png"
+    growl "Test Results", "#{total} examples, #{failures} failures", image_path, 0
   end
   
   def growl(title, msg, img, pri = 0)
